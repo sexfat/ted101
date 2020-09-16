58fe508a77e3e6c7277601664b56ff8c3b9b6010
sexfat <bryant6831@gmail.com>
2020-09-16 15:34:38 +0800

    add beizer

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 TweenMax.to('.b1', .8, {
     //x: 400,
-    ease:  Power0.easeNone,
+    ease: Power0.easeNone,
     // repeat: -1,//重複
     //yoyo: true //方向
     // scale: 2, //放大
@@ -10,19 +10,47 @@ TweenMax.to('.b1', .8, {
     // rotation : 360, //旋轉
     // rotationX : 360, //旋轉
     // rotationY : 360, //旋轉
-    transformOrigin : 'left top', // top  right left bottom  10% 20% 
-    backgroundColor  : '#999',
+    transformOrigin: 'left top', // top  right left bottom  10% 20% 
+    backgroundColor: '#999',
     // border : '1px solid #f20' ,
-    width : 900,
-    borderRadius : '10px 10px'
+    width: 900,
+    borderRadius: '10px 10px'
 });
 
 TweenMax.to('.b2', 1, {
-    y: 100,
-    ease:  Bounce.easeOut, 
+    y: 200,
+    ease: Bounce.easeOut,
 });
 
-TweenMax.fromTo('.b3', 1,{x: 100},{x:400});
+TweenMax.fromTo('.b3', 1, {
+    x: 100
+}, {
+    x: 400
+});
 
-
-
+TweenMax.to('.b4', 10, {
+    bezier: {
+        curviness: 1.25,
+        values: [{
+                x: 100,
+                y: 400
+            },
+            {
+                x: 250,
+                y: 400
+            },
+            {
+                x: 300,
+                y: 500
+            },
+            {
+                x: 500,
+                y: 500
+            },
+            {
+                x: 0,
+                y: 0
+            }
+        ]
+    }
+})
