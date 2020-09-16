6427b29db7174b0c948558543bf8cf2b112341c0
sexfat <bryant6831@gmail.com>
2020-09-16 14:41:49 +0800

    add

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-TweenMax.to('.b1', 1, {
-    y: 100,
-    x: 100
+TweenMax.to('.b1', 2, {
+    x: 400,
+    //ease:  Bounce.easeOut, 
+    //repeat: -1,//重複
+    //yoyo: true //方向
+    //scale: 2, //放大
+    // opacity: 0 //透明度
 });
 
-TweenMax.from('.b2', 1, {
-    y: 100
+TweenMax.to('.b2', 1, {
+    y: 100,
+    ease:  Bounce.easeOut, 
 });
+
+TweenMax.fromTo('.b3', 1,{x: 100},{x:400});
 
 
 
