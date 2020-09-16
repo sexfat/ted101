058afb569a4fb1a0522a79c35dcb92446a14d3bc
sexfat <bryant6831@gmail.com>
2020-09-16 15:25:49 +0800

    add

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 TweenMax.to('.b1', .8, {
     //x: 400,
     ease:  Power0.easeNone,
-    repeat: -1,//重複
+    // repeat: -1,//重複
     //yoyo: true //方向
-    //scale: 2, //放大
+    // scale: 2, //放大
+    //scaleX: 2, //放大
+    //scaleY: 2, //放大
     // opacity: 0 //透明度
     // rotation : 360, //旋轉
-    rotationX : 360, //旋轉
-    transformOrigin : 'right bottom', // top  right left bottom  10% 20% 
+    // rotationX : 360, //旋轉
+    // rotationY : 360, //旋轉
+    transformOrigin : 'left top', // top  right left bottom  10% 20% 
+    backgroundColor  : '#999',
+    // border : '1px solid #f20' ,
+    width : 900,
+    borderRadius : '10px 10px'
 });
 
 TweenMax.to('.b2', 1, {
