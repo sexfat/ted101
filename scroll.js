bbd126120f504a5b9e5abae6bb3a9dfd674687ba
sexfat <bryant6831@gmail.com>
2020-09-23 15:18:20 +0800

    add

--- scroll.js
+++ scroll.js
@@ -8,23 +8,30 @@ var mv01 = TweenMax.fromTo('.mg', 1, {
 }, {
     x: 400,
     opacity: 1
-})
+});
 
-//觸發事件
+//場景一觸發事件
 var scene01 = new ScrollMagic.Scene({
     triggerElement: '#point1',
     triggerHook: 0
-}).setTween(mv01).addIndicators().addTo(controller);
+}).setTween(mv01).addIndicators().addTo(controller)
 
+
+//動畫
 var t1 = new TimelineMax();
 
 t1.to('.mv01', 1, {
     x: 100
 }).to('.mv02', 1, {
     x: 200
+}).to('.mv03', 1, {
+    x: 300
+}).to('.mv04', 1, {
+    x: 500
 })
 
-
+//場景二觸發事件
 var scene02 = new ScrollMagic.Scene({
     triggerElement: '#point2',
+    reverse: false
 }).setTween(t1).addIndicators().addTo(controller);
