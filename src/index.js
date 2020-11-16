f583eabd73e7a5f6ddc56ca377963ba9980b8c0f
sexfat <bryant6831@gmail.com>
2020-11-16 16:18:51 +0800

    add css loader

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 import { TweenMax } from "gsap";
+import './style.css'
 
 
 
 
-$('body').css('background-color' , "red");
 
-$('h1').css('color' , "white");
+// $('body').css('background-color' , "red");
 
-TweenMax.to('.h1' , 1 , {
-    x: 900,
+// $('h1').css('color' , "white");
+
+TweenMax.to('h1' , 1 , {
+    x: 100,
     y: 100
 })
 
-function square(num) {
-    return num * num
-}
+// function square(num) {
+//     return num * num
+// }
 
-console.log(square(10));
+// console.log(square(10));
