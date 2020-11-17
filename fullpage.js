ea79cf1b0121ac3df25079f535889a128a5b20c6
sexfat <bryant6831@gmail.com>
2020-11-17 09:45:57 +0800

    add

--- fullpage.js
+++ fullpage.js
@@ -1,18 +1,24 @@
 new fullpage('#fullpage', {
     //options here
-    anchors: ['firstPage' , 'secondPage' , 'thirdPage', 'lastPage'],
-	autoScrolling:true,
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'lastPage'],
+    autoScrolling: true,
     scrollHorizontally: true,
-    afterLoad: function(origin, destination, direction) {
-        if(origin.index == 0 && direction =='down'){
-            TweenMax.fromTo('.box' , 1 , {
-                opacity : 0,
+    afterLoad: function (origin, destination, direction) {
+        if (origin.index == 0 && direction == 'down') {
+            //section2
+            TweenMax.fromTo('.box', 1, {
+                opacity: 0,
                 x: -100
-              },{
-                 opacity: 1,
-                 x: 100
-              })
-		}
-       
+            }, {
+                opacity: 1,
+                x: 100
+            })
+        }
+        else if (origin.index == 3) {
+            //section4
+            alert('ok')
+        }
+
+
     }
 });
