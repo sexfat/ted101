7622dfa175ba269cb4342da7f10380c48919ab5a
sexfat <bryant6831@gmail.com>
2020-11-17 11:23:19 +0800

    加入gulp 分類

--- gulpfile.js
+++ gulpfile.js
@@ -6,61 +6,32 @@ const {
     watch
 } = require('gulp');
 
-const babel = require('gulp-babel');
+
+
+
+//把模組載入
+
+const clean = require('./tasks/clean');
+const styles = require('./tasks/sassStyle');
+const html = require('./tasks/html_template');
+const jsBabel = require('./tasks/jsBabel');
+const concats = require('./tasks/concat');
+const image = require('./tasks/image');
+const server = require('./tasks/server');
+
+
 
 
 
 // 第一個任務
 
 
-function p01(cb) {
-    console.log('任務一');
-    cb();
-}
+// exports.all = series(p01, p02); //依序執行
+// exports.all2 = parallel(p01, p02); //並行
 
 
-function p02(cb) {
-    console.log('任務二');
-    cb();
-}
-
-exports.one = p01; // exports 輸出指令 do 
-exports.two = p02; // exports 輸出指令 do 
 
 
-exports.all = series(p01, p02); //依序執行
-exports.all2 = parallel(p01, p02); //並行
-
-// 搬家任務
-exports.move = function movefile() {
-    return src('index.html')
-        .pipe(dest('app/'))
-}
-
-// 合併檔案任務
-
-const concat = require('gulp-concat');
-const cleanCSS = require('gulp-clean-css');
-const rename = require("gulp-rename");
-
-exports.concatCSS = function concatcss() {
-    return src(['css/*.*', '!css/scripts.js'])
-        .pipe(concat('all.css'))
-        .pipe(cleanCSS())
-        .pipe(rename(function (path) {
-            path.basename += "-min";
-            path.extname = ".css";
-        }))
-        .pipe(dest('app/'))
-}
-
-// function minicss() {
-//     return src('app/*.css')
-//     .pipe(cleanCSS({compatibility: 'ie8'}))
-//     .pipe(dest('app/mini'));
-// }
-
-//exports.doall = series(concatcss , minicss);
 
 
 //排除 用!*.js   排除單個檔案
@@ -70,130 +41,129 @@ exports.concatCSS = function concatcss() {
 // 壓縮css 
 // https://www.npmjs.com/package/gulp-clean-css
 
-exports.name = function Rename() {
-    return src('app/all.css')
-        .pipe(rename(function (path) {
-            path.basename += "aa";
-            path.extname = ".css";
-        })).pipe(dest('app/'))
-}
+// exports.name = function Rename() {
+//     return src('app/all.css')
+//         .pipe(rename(function (path) {
+//             path.basename += "aa";
+//             path.extname = ".css";
+//         })).pipe(dest('app/'))
+// }
 
 
-//gulp sass
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-
-function styleSass() {
-    return src('./sass/*.scss') //input路徑
-        .pipe(sass({
-            outputStyle: "compressed" //壓縮css 
-        }).on('error', sass.logError))
-        //.pipe(autoprefixer())
-        // .pipe(rename(function (path) {
-        //     path.basename += "-prefix";
-        //     path.extname = ".css";
-        //   })) //改名
-        .pipe(dest('./app/css/')) //output路徑
-}
-
-// html 樣版
-const fileinclude = require('gulp-file-include');
+// //gulp sass
 
 
-function htmlTemplate() {
-    return src('./*.html') //input
-        .pipe(fileinclude({
-            prefix: '@@',
-            basepath: '@file'
-        })).pipe(dest('./app/')) //output
-}
+// function styleSass() {
+//     return src('./sass/*.scss') //input路徑
+//         .pipe(sass({
+//             outputStyle: "compressed" //壓縮css 
+//         }).on('error', sass.logError))
+//         //.pipe(autoprefixer())
+//         // .pipe(rename(function (path) {
+//         //     path.basename += "-prefix";
+//         //     path.extname = ".css";
+//         //   })) //改名
+//         .pipe(dest('./app/css/')) //output路徑
+// }
+
+// // html 樣版
 
 
 
-//監看檔案 -> 輸出
-
-exports.watch = function watchfile() {
-    watch('./sass/*.scss', styleSass); // 執行function
-    watch(['./*.html', './**/*.html', '!app/*.html'], htmlTemplate) // 執行function
-}
-// 瀏覽器同步
-
-const browserSync = require('browser-sync');
-const reload = browserSync.reload;
-
-exports.default = function browser() {
-    browserSync.init({
-        server: {
-            baseDir: "./app",
-            index: 'index.html'
-        },
-        //port: 3200
-    });
-    watch('./sass/*.scss', styleSass).on('change', reload)
-    watch(['./*.html', './**/*.html', '!app/*.html'], htmlTemplate).on('change', reload)
-}
+// function htmlTemplate() {
+//     return src('./*.html') //input
+//         .pipe(fileinclude({
+//             prefix: '@@',
+//             basepath: '@file'
+//         })).pipe(dest('./app/')) //output
+// }
 
 
-//  prefix -> 改名 -> 壓縮css 
-// exports.packagecss = () => (
-//     src('app/css/*.css')
+
+// //監看檔案 -> 輸出
+
+// exports.watch = function watchfile() {
+//     watch('./sass/*.scss', styleSass); // 執行function
+//     watch(['./*.html', './**/*.html', '!app/*.html'], htmlTemplate) // 執行function
+// }
+// // 瀏覽器同步
+
+// const browserSync = require('browser-sync');
+// const reload = browserSync.reload;
+
+// exports.default = function browser() {
+//     browserSync.init({
+//         server: {
+//             baseDir: "./app",
+//             index: 'index.html'
+//         },
+//         //port: 3200
+//     });
+//     watch('./sass/*.scss', styleSass).on('change', reload)
+//     watch(['./*.html', './**/*.html', '!app/*.html'], htmlTemplate).on('change', reload)
+// }
+
+
+// //  prefix -> 改名 -> 壓縮css 
+// // exports.packagecss = () => (
+// //     src('app/css/*.css')
+// //         .pipe(autoprefixer())
+// //          .pipe(rename(function (path) {
+// //             path.basename += "_prefix";
+// //             path.extname = ".css";
+// //           })) //改名
+// //         .pipe(cleanCSS({compatibility: 'ie8'})) 
+// //         .pipe(dest('app/css/prefix'))
+// // );
+
+
+
+// function prefix() {
+//     return src('app/css/*.css')
 //         .pipe(autoprefixer())
-//          .pipe(rename(function (path) {
+//         .pipe(rename(function (path) {
 //             path.basename += "_prefix";
 //             path.extname = ".css";
-//           })) //改名
-//         .pipe(cleanCSS({compatibility: 'ie8'})) 
+//         })) //改名
+//         .pipe(cleanCSS({
+//             compatibility: 'ie8'
+//         }))
 //         .pipe(dest('app/css/prefix'))
-// );
+// }
 
 
+// //圖片壓縮
 
-function prefix() {
-    return src('app/css/*.css')
-        .pipe(autoprefixer())
-        .pipe(rename(function (path) {
-            path.basename += "_prefix";
-            path.extname = ".css";
-        })) //改名
-        .pipe(cleanCSS({
-            compatibility: 'ie8'
-        }))
-        .pipe(dest('app/css/prefix'))
-}
-
-
-//圖片壓縮
-
-const imagemin = require('gulp-imagemin');
-// exports.img = () => (
-//     src('images/*')
+// const imagemin = require('gulp-imagemin');
+// // exports.img = () => (
+// //     src('images/*')
+// //         .pipe(imagemin())
+// //         .pipe(dest('app/images'))
+// // );
+// function img() {
+//     return src('images/*')
 //         .pipe(imagemin())
 //         .pipe(dest('app/images'))
-// );
-function img() {
-    return src('images/*')
-        .pipe(imagemin())
-        .pipe(dest('app/images'))
-}
+// }
 
-const uglify = require('gulp-uglify');
+// const uglify = require('gulp-uglify');
 
-function babeles5() {
-    return src('js/*.js')
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(uglify())
-        .pipe(dest('app/js'))
-}
-//清除舊有檔案
-var clean = require('gulp-clean');
+// function babeles5() {
+//     return src('js/*.js')
+//         .pipe(babel({
+//             presets: ['@babel/env']
+//         }))
+//         .pipe(uglify())
+//         .pipe(dest('app/js'))
+// }
+// //清除舊有檔案
+// var clean = require('gulp-clean');
 
-function clear() {
-  return src('app' ,{ read: false })
-  .pipe(clean({force: true}));
-}
+// function clear() {
+//   return src('app' ,{ read: false })
+//   .pipe(clean({force: true}));
+// }
 
 
-//打包
-exports.packageAll = series(clear,styleSass,parallel(htmlTemplate ,prefix, img, babeles5));
+// //打包
+// exports.packageAll = series(clear,styleSass,parallel(htmlTemplate ,prefix, img, babeles5));
--- js/a.js
+++
@@ -1,5 +0,0 @@
-const callsomeone = (someone) => {
-   return someone + '做事情' 
-}
-
-console.log(callsomeone('小明'))
