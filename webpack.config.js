75dc24979680b8010b59cf1b64470f8d87ecbcc4
sexfat <bryant6831@gmail.com>
2020-11-18 10:00:10 +0800

    add scss

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     module: {
         rules: [{
             // 格式
-            test: /\.css$/,
+            test: /\.(sass|scss|css)$/,
             //順序是由下到上 css > style
             use: [{
                 loader: MiniCssExtractPlugin.loader,
@@ -20,7 +20,8 @@ module.exports = {
                 }
               },
                 //'style-loader', 會跟原本的衝突 
-                'css-loader'
+                'css-loader',
+                'sass-loader'
             ],
         }]
 
